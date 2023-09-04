3be0782eacbbfde086ef8c0cf89572eb9899825a
biniam-bin <biniamdnl@gmail.com>
2023-09-04 13:16:32 -0400

    fix: yarn error

--- src/Screen/Register/index.jsx
+++ src/Screen/Register/index.jsx
@@ -1,28 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
 import { Parallax } from "react-parallax";
-import { RegisterContainer } from './style';
-import Link from 'next/link';
-
-
+import { RegisterContainer } from "./style";
+import Link from "next/link";
 
 export const RegisterScreen = () => {
+
   return (
     <RegisterContainer>
       <div className="box">
         <h1>Register</h1>
         <form action="">
-          <input type="text" placeholder='E-mail' />
-          <input type="password" placeholder='password' />
-          <input type="password" placeholder='password(again)' />
+          <input type="text" placeholder="E-mail" />
+          <input type="password" placeholder="password" />
+          <input type="password" placeholder="password(again)" />
           <div className="btn">Login</div>
         </form>
         <div className="footer">
-
-        <p>already a customer?</p>
-        <Link href="/login"><span>Register</span></Link>
+          <p>already a customer?</p>
+          <Link href="/login">
+            <span>Register</span>
+          </Link>
         </div>
-     </div>
+      </div>
     </RegisterContainer>
   );
-}
-
+};
